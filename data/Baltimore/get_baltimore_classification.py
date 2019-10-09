43099e8f7c8e88746bf383eee84253a299cd7233
horaciobam <laura.baenam@gmail.com>
2019-10-09 13:04:41 -0400

    relating baltimore info with virus info

--- data/Baltimore/get_baltimore_classification.py
+++ data/Baltimore/get_baltimore_classification.py
@@ -1,5 +1,5 @@
 # Clasify viruses according to their type of molecule (dsDNA, ssDNA, dsRNA, RT-RNA, (+)ss-RNA, (-)ssRNA, (circular)ssRNA)
-import os
+import csv
 #dir = '../data/Baltimore'
 
 filename_list = ("baltimore_cirucular_ssRNA.txt", "baltimore_RTviruses.txt",
@@ -26,4 +26,23 @@ for filename in filename_list:
                 #     print(line)
                 # TODO: include viruses with unsassigned families
         virus_dict[key] = families
+
+
+# Import virus table
+species_file_name = "virus_full_info.txt"
+# species_file = open(species_file_name, "r")
+# line_s = species_file.readlines()
+# for line in line_s:
+#     print(line)
+
+
+with open(species_file_name) as file:
+    csv_reader = csv.reader(file, delimiter = ',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            for colnames in row:
+                print (colnames)
+
+
 
