92ceeff42cc7b8972069a13c6bf88192715aa39c
horaciobam <laura.baenam@gmail.com>
2019-10-18 16:52:46 -0400

    ggplot fot ridgeplot

--- data/Baltimore/improving_figures.R
+++ data/Baltimore/improving_figures.R
@@ -4,7 +4,8 @@ virus <- read.csv('species_file_baltimore2.csv')
 
 
 #stacked bar plot
-more_than_zero <- subset(virus, len.overlaps > 0)
+no_na <- na.omit(virus)
+more_than_zero <- subset(no_na, len.overlaps > 0)
 pal <- c("#f1d4d4", "#ddb6c6", "#ac8daf", "#484c7f")
 counts <- table(more_than_zero$family, more_than_zero$baltimore.class)
 matrix <- as.matrix(counts)
@@ -41,6 +42,7 @@ devtools::install_github("ArtPoon/ggfree")
 
 library(ggplot2)
 library(ggridges)
+library(gridExtra)
 
 test <- virus
 test$subset <- NA
@@ -91,16 +93,27 @@ ggplot(test2, aes(x = n.overlaps, y = subset, group = subset)) +
   geom_density_ridges(scale = 9)
 
 
-########################################
-# Ridgeplot for Medium overlap length
-########################################
+#############################################
+# Ridgeplot for Medium overlap length PAGE 1
+############################################
+pdf(file='new_figures.pdf', width=9, height=13, title = "Analysis of overlappin genes in viral genomes")
+par(mar = c(4,4,3,4), mfrow=c(4,1))
+
+# Short overlaps
 more_than_zero$ov.len.class[more_than_zero$len.overlaps < 1] <- "Zero overlaps"
 more_than_zero$ov.len.class[1 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 10] <- "1 to 10"
 more_than_zero$ov.len.class[10 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 20] <- "10 to 20"
 more_than_zero$ov.len.class[20 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 30] <- "20 to 30"
 more_than_zero$ov.len.class[30 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 40] <- "30 to 40"
 more_than_zero$ov.len.class[40 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 50] <- "40 to 50"
-more_than_zero$ov.len.class[50 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 100] <- "50 to 100"
+more_than_zero$ov.len.class[50 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 60] <- "50 to 600"
+more_than_zero$ov.len.class[60 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 70] <- "60 to 70"
+more_than_zero$ov.len.class[70 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 80] <- "70 to 80"
+more_than_zero$ov.len.class[80 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 90] <- "80 to 90"
+more_than_zero$ov.len.class[90 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 100] <- "90 to 100"
+
+
+# Long overlaps
 more_than_zero$ov.len.class[100 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 200] <- "100 to 200"
 more_than_zero$ov.len.class[200 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 300] <- "200 to 300"
 more_than_zero$ov.len.class[300 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 400] <- "300 to 400"
@@ -110,38 +123,102 @@ more_than_zero$ov.len.class[600 <= more_than_zero$len.overlaps & more_than_zero$
 more_than_zero$ov.len.class[700 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 800] <- "700 to 800"
 more_than_zero$ov.len.class[800 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 900] <- "800 to 900"
 more_than_zero$ov.len.class[900 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 1000] <- "900 to 1000"
-more_than_zero$ov.len.class[1000 < more_than_zero$len.overlaps] <- "> 1000"
+#more_than_zero$ov.len.class[1000 < more_than_zero$len.overlaps] <- "> 1000"
+
+# Very long overlaps
+more_than_zero$ov.len.class[1000 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 5000] <- "1000 to 5000"
+more_than_zero$ov.len.class[5000 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 10000] <- "5000 to 10000"
+more_than_zero$ov.len.class[10000 <= more_than_zero$len.overlaps] <- "> 10000"
+
+# Everything
+# ggplot(more_than_zero, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
+#   geom_density_ridges()
 
 
-
-ggplot(more_than_zero, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
-  geom_density_ridges()
-
-
-ggplot(more_than_zero, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
+all<- ggplot(more_than_zero, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
   geom_density_ridges(
     aes(point_color = ov.len.class, point_fill = ov.len.class, point_shape = ov.len.class),
     alpha = .2, point_alpha = 1, jittered_points = TRUE
   ) +
   scale_point_color_hue(l = 40) +
-  scale_discrete_manual(aesthetics = "point_shape", values = c(18,17,19, 20, 21, 22, 23, 25, 25))
+  scale_discrete_manual(aesthetics = "point_shape", values = c(21, 22, 23, 25, 25, 21, 22,
+                                                               23, 25, 25, 21, 22, 23, 25, 25, 21, 22, 23, 25, 25, 21, 22, 23))+
+  ggtitle("Medium overlap length (all data base)")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  labs(y="Category", x = "Medium overlap length (nts)")
 
+#Plotting small
 small <- subset(more_than_zero, len.overlaps < 100 & len.overlaps != 0)
-
-ggplot(small, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
+s <- ggplot(small, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
   geom_density_ridges(
     aes(point_color = ov.len.class, point_fill = ov.len.class, point_shape = ov.len.class),
     alpha = .2, point_alpha = 1, jittered_points = TRUE
   ) +
   scale_point_color_hue(l = 40) +
-  scale_discrete_manual(aesthetics = "point_shape", values = c(18,17,19, 20, 21, 22, 23, 25, 25))
+  scale_discrete_manual(aesthetics = "point_shape", values = c(21, 22, 23, 25, 25, 21, 22, 23, 25, 25))+
+  ggtitle("Small overlaps (< 100 nucletotides)")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  labs(y="Category", x = "Medium overlap length (nts)")
 
+
+#Plotting large
 large <- subset(more_than_zero, len.overlaps > 100 & len.overlaps <1000)
-ggplot(large, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
+l<- ggplot(large, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
   geom_density_ridges(
     aes(point_color = ov.len.class, point_fill = ov.len.class, point_shape = ov.len.class),
     alpha = .2, point_alpha = 1, jittered_points = TRUE
   ) +
   scale_point_color_hue(l = 40) +
-  scale_discrete_manual(aesthetics = "point_shape", values = c(18,17,19, 20, 21, 22, 23, 25, 25, 16))
+  scale_discrete_manual(aesthetics = "point_shape", values = c(21, 22, 23, 25, 25, 21, 22, 23, 25, 25))+
+  ggtitle("Long overlaps (100 < nucletotides < 1000)")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  labs(y="Category", x = "Medium overlap length (nts)")
 
+#Plotting very large
+very_large <- subset(more_than_zero, len.overlaps >= 1000)
+v_l <-ggplot(very_large, aes(x = len.overlaps, y = ov.len.class, group = ov.len.class)) + 
+  geom_density_ridges(
+    aes(point_color = ov.len.class, point_fill = ov.len.class, point_shape = ov.len.class),
+    alpha = .2, point_alpha = 1, jittered_points = TRUE
+  ) +
+  scale_point_color_hue(l = 40) +
+  scale_discrete_manual(aesthetics = "point_shape", values = c(21, 22, 23, 25, 25, 21, 22, 23, 25, 25))+
+  ggtitle("Very long overlaps (< 1000 nucleotides)")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  labs(y="Category", x = "Medium overlap length (nts)")
+grid.arrange(all, s, l, v_l, nrow = 4)
+
+dev.off()
+########################################
+# Ridgeplot for Number of proteins
+########################################
+# Short overlaps
+more_than_zero$ov.len.class[more_than_zero$len.overlaps < 1] <- "Zero overlaps"
+more_than_zero$ov.len.class[1 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 10] <- "1 to 10"
+more_than_zero$ov.len.class[10 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 20] <- "10 to 20"
+more_than_zero$ov.len.class[20 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 30] <- "20 to 30"
+more_than_zero$ov.len.class[30 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 40] <- "30 to 40"
+more_than_zero$ov.len.class[40 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 50] <- "40 to 50"
+more_than_zero$ov.len.class[50 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 60] <- "50 to 600"
+more_than_zero$ov.len.class[60 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 70] <- "60 to 70"
+more_than_zero$ov.len.class[70 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 80] <- "70 to 80"
+more_than_zero$ov.len.class[80 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 90] <- "80 to 90"
+more_than_zero$ov.len.class[90 <= more_than_zero$len.overlaps & more_than_zero$len.overlaps < 100] <- "90 to 100"
+
+v_l <-ggplot(very_large, aes(x = len.overlaps, y = family, group = family)) + 
+  geom_density_ridges(
+    aes(point_color = family, point_fill = family, point_shape = family),
+    alpha = .2, point_alpha = 1, jittered_points = TRUE
+  ) +
+  scale_point_color_hue(l = 40) +
+  scale_discrete_manual(aesthetics = "point_shape", values = c(21, 22, 23, 25, 25, 21, 22, 23, 25, 25, 21, 22, 23, 25, 25, 21, 22, 23, 25, 25, 25))+
+  ggtitle("Very long overlaps (< 1000 nucleotides)")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  labs(y="Category", x = "Medium overlap length (nts)")
+grid.arrange(all, s, l, v_l, nrow = 4)
+
+########################################
+# Earth movers distance
+########################################
+library(emdist)
+len_by_fam <- split(more_than_zero$len.overlaps, more_than_zero$family)
