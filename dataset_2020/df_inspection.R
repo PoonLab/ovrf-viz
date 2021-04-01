769b9c7f61216f877f589a564d02b1e135bfbd9c
horaciobam <laura.baenam@gmail.com>
2021-04-01 18:39:41 -0400

    Re joining datasets after overlap re-calculation

--- dataset_2020/df_inspection.R
+++ dataset_2020/df_inspection.R
@@ -1,4 +1,6 @@
-# Inspecting data frames
+########################################################
+# Plot frame shift distribution
+########################################################
 setwd('/home/lmunoz/Projects/ovrf-review/dataset_2020/')
 overlaps <- read.csv('2_overlapping_march2020.csv', colClasses='character')
 
@@ -28,9 +30,6 @@ non_splicing_overlaps <- overlaps %>%
 #       ((shift == "+1" | shift == "-1") & overlap%%3 == 2) |
 #       ((shift == "+2" | shift == "-2") & overlap%%3 == 1),
 #   )
-
-
-orfs <- read.csv('total_orfs.csv')
 
 # Ridgeplot
 plot <- ggplot(non_splicing_overlaps, aes(x = log10(overlap), y = shift, group = shift, fill = shift)) +
@@ -89,3 +88,62 @@ treemap(sub, index = "shift", vSize = "Freq", type="index",
         palette=new_p, title="RT_viruses", fontsize.title=15, fontsize.labels=14,
         vp = vplayout(3,2))
 
+########################################################
+# Import and merge data
+########################################################
+setwd('/home/lmunoz/Projects/ovrf-review/dataset_2020/')
+overlaps <- non_splicing_overlaps
+orfs <- read.csv('total_orfs.csv')
+virus <- read.csv('try_baltimore_class.csv')
+
+virus$Proteins[virus$Proteins=='-'] <- NA
+virus$Proteins <- as.integer(as.character(
+  virus$Proteins))
+summary(virus)
+
+
+# the number of ORFs increases linearly with genome size
+plot(virus$Length, virus$Proteins, log='x')
+
+temp <- sapply(split(overlaps$overlap, overlaps$Accession), length)
+noverlaps <- data.frame(accn=names(temp), count=temp)
+
+noverlaps$mean.olen <- sapply(split(overlaps$overlap, overlaps$Accession), mean)
+noverlaps$total.olen <- sapply(split(overlaps$overlap, overlaps$Accession), sum)
+
+# sum total number of overlaps per genome
+virus$first.acc <- sapply(virus$Representative, function(x) {
+  xx <- as.character(x)
+  if (grepl("^\\[", xx)) {
+    gsub("^\\['([A-Z0-9_]+)'.+", "\\1", xx)
+  } else {
+    xx
+  }
+})
+
+index <- match(virus$Representative, noverlaps$accn)
+virus$n.overlaps <- noverlaps$count[index] # 
+virus$total.overlaps<-noverlaps$total.olen[index] #Total number of overlaps
+
+
+# carry over Genome to <noverlaps>
+all.acc <- sapply(1:nrow(virus), function(i) {
+  xx <- as.character(virus$Representative[i])
+  if (grepl("^\\[", xx)) {
+    x2 <- gsub("[\\]'\\[]", "", xx, perl=T)
+    strsplit(x2, ", ")[[1]]
+  } else {
+    xx
+  }
+})
+
+all.acc <- data.frame(acc=unlist(all.acc), genome=rep(virus$Taxonomy.name, times=sapply(all.acc, length)))
+
+index2 <- match(noverlaps$accn, all.acc$acc)
+noverlaps$genome <- all.acc$genome[index2]
+
+temp <- sapply(split(noverlaps$mean.olen/noverlaps$count, noverlaps$genome), mean)
+index3 <- match(virus$Taxonomy.name, names(temp))
+virus$len.overlaps <- ifelse(virus$n.overlaps==0, NA, temp[index3] * virus$n.overlaps)
+
+write.csv(virus, "out_virus_df_inspection.csv")
